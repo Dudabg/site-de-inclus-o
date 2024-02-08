dad3777e02e05429ce0eacbd853d91a9b75f8394
Dudabg <116316223+Dudabg@users.noreply.github.com>
2024-02-08 09:42:16 -0300

    up

---
+++ javaScript.js
@@ -0,0 +1,13 @@
+/* Popup Cadastro de Membros */
+/* Funções para abrir e fechar popup */
+
+function abrirPopup(popupId) {
+        document.getElementById(popupId).style.display = 'block';
+    }
+    
+    function fecharPopup(popupId) {
+        document.getElementById(popupId).style.display = 'none';
+    }
+    
+    /* Fim Popup Cadastro de Membros */
+    
